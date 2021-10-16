c1639e289f0378efd22cddb7cddd79c517d23ca1
Arunas Slepikas <dijitle@gmail.com>
2021-10-16 16:06:16 -0500

    adding map, some flex layout stuff

--- src/App.js
+++ src/App.js
@@ -4,30 +4,61 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  const [rows, setRows] = React.useState([]);
-  const [columns, setColumns] = React.useState(["Name"]);
+  const [cities, setCities] = React.useState([
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+    "Dallas",
+    "Fort Worth",
+    "Denver",
+    "Miami",
+    "Philadelphia",
+    "Austin",
+    "San Antonio",
+    "Aurora",
+    "Indianapolis",
+    "Chesteron",
+    "Portage",
+    "New Haven",
+    "Boston",
+    "Providence",
+    "Lansing",
+    "Columbus",
+    "Honolulu",
+    "Boise",
+    "Nashville",
+    "Memphis",
+    "Knoxville",
+    "Chattanooga",
+  ]);
 
-  React.useEffect(() => {
-    async function getData() {
-      const response = await fetch("/data/uscities.csv");
-      const reader = response.body.getReader();
-      const result = await reader.read(); // raw array
-      const decoder = new TextDecoder("utf-8");
-      const csv = decoder.decode(result.value); // the csv text
-      const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
-      const rows = results.data; // array of objects
-      setRows(rows);
-    }
-    getData();
-  }, []); // [] means just do this once, after initial render
   return (
-    <div className="App container-fluid">
-      <input
-        className="CityInput fs-3 fw-bold m-3 p-3 bg-dark bg-gradient text-white"
-        type="text"
-        spellcheck="false"
-        placeholder="Enter city name"
-      />
+    <div className="App">
+      <div className="InputMap">
+        <input
+          className="CityInput fs-3 fw-bold m-3 p-3 bg-dark bg-gradient text-white"
+          type="text"
+          spellcheck="false"
+          placeholder="Enter city name"
+        />
+        <img className="Map" alt="map" src="usa.svg" />
+      </div>
+      <div className="MyCities">
+        <ul>
+          {cities.map((city) => (
+            <li>{city}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="MyCities">
+        <ul>
+          {cities.map((city) => (
+            <li>{city}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
